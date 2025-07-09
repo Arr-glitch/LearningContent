f5bb1bb62e57001a64149e37fe926d62d697dec0
Interactive English Learning Team <developer@interactive-english-learning.com>
2025-07-09 21:48:06 +0300

    Update firebase-config.js

--- assets/firebase-config.js
+++ assets/firebase-config.js
@@ -1,10 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBJfpytCRSfrV8JhGGGWGyCH3sa3B83j0M",
+  authDomain: "english--content-book.firebaseapp.com",
+  projectId: "english--content-book",
+  storageBucket: "english--content-book.firebasestorage.app",
+  messagingSenderId: "110614867380",
+  appId: "1:110614867380:web:2335bde59c925b02b4d6e1",
+  measurementId: "G-CC8DCB0ZQE"
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
